547f6b01a38d16d5a5b5b4101a3d250f4a77218b
Autotrato3964 <comercial@autotrato.com.ar>
2026-07-04 20:38:57 -0300

    Primera versión del CRM

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'autotrato-crm-v1';
+const ARCHIVOS_BASE = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ARCHIVOS_BASE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((nombres) =>
+      Promise.all(nombres.filter((n) => n !== CACHE_NAME).map((n) => caches.delete(n)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Estrategia: red primero (para tener siempre los datos más nuevos),
+// y si no hay conexión, usa lo último guardado en caché.
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((respuesta) => {
+        const copia = respuesta.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copia));
+        return respuesta;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
